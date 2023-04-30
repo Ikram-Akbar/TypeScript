e8f46ca2e1eccba582c6758cedbb49cae3c40dd0
Ikram-Git-Coder <ikramakbar71@gmail.com>
2023-04-30 20:05:10 +0600

    init the typeScipt

---
+++ output/root.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("hello typescript");
+const lagName = " Typescript";
+const agName = " JavaScript";
+console.log(`one of my favorite programming language is ${lagName}`);
+console.log(` programming language is ${agName}`);
---
+++ src/root.ts
@@ -0,0 +1,5 @@
+console.log("hello typescript");
+ const lagName:string = " Typescript";
+ const agName:string = " JavaScript";
+console.log(`one of my favorite programming language is ${lagName}`);
+console.log(` programming language is ${agName}`);
