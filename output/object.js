a0853f6ed3730acc3da58b4518b85b1d9a378ab4
Ikram-Git-Coder <ikramakbar71@gmail.com>
2023-04-30 22:46:02 +0600

    object in ts

---
+++ output/object.js
@@ -0,0 +1,8 @@
+"use strict";
+// create an object
+const player = {
+    fName: "Mashrafi",
+    age: 38,
+    isPlay: false,
+    tShirtNo: 2
+};
---
+++ src/object.ts
@@ -0,0 +1,15 @@
+
+// create an object
+
+const player: {
+    fName: string;
+    age: number;
+    isPlay: boolean;
+    tShirtNo?: number;
+} = {
+    fName: "Mashrafi",
+    age: 38,
+    isPlay: false,
+    tShirtNo: 2
+}
+
