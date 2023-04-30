81c436174b3e9ee97b3a46f410a066f1a7d493cf
Ikram-Git-Coder <ikramakbar71@gmail.com>
2023-04-30 21:32:11 +0600

    update the readme file and Premitve data type

--- output/root.js
+++ output/root.js
@@ -2,5 +2,6 @@
 console.log("hello typescript");
 const lagName = " Typescript";
 const agName = " JavaScript";
+let age = 25;
 console.log(`one of my favorite programming language is ${lagName}`);
-console.log(` programming language is ${agName}`);
+console.log(` programming language is ${agName} and ${age}`);
