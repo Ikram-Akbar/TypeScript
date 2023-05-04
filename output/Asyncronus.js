6d3dc361f671567023660c93d72221814f1f102d
Ikram-Git-Coder <ikramakbar71@gmail.com>
2023-05-04 22:56:26 +0600

    asyncronus

--- output/Asyncronus.js
+++ output/Asyncronus.js
@@ -8,7 +8,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-// promise handle in ts
+// promise handle in ts 
 //string in promise ----- 
 const makePromise = () => {
     return new Promise((resolve, reject) => {
@@ -23,6 +23,29 @@ const makePromise = () => {
 };
 const getPromiseData = () => __awaiter(void 0, void 0, void 0, function* () {
     const data = yield makePromise();
-    return data;
     console.log(data);
+    return data;
 });
+getPromiseData();
+//-------------  //  ------------ // -----------
+//boolean 
+const createBoolPromise = () => {
+    return new Promise((res, rej) => {
+        const isOnline = true;
+        if (isOnline) {
+            console.log(`Promise has been resolved -- ${isOnline}`);
+            res(isOnline);
+        }
+        else {
+            console.log("Field to load the data ");
+            rej();
+        }
+    });
+};
+const myBool = () => __awaiter(void 0, void 0, void 0, function* () {
+    const newData = yield createBoolPromise();
+    return newData;
+});
+function fullName(person) {
+    return `${person.firstName} ${person.lastName}`;
+}
