636413886334896b45eb31b61cb6bb5c01cc5c2d
Ikram-Git-Coder <ikramakbar71@gmail.com>
2023-05-05 04:16:53 +0600

    inharitance

--- src/OOP/inharitance.ts
+++ src/OOP/inharitance.ts
@@ -1,12 +1,23 @@
 //make a parent class :
 class Root {
-  constructor(
-    public fName: string,
-    public lName: string,
-    public age: number,
-    public isMarried?: boolean
-  ) {}
+    constructor(
+        public fName: string,
+        public lName: string,
+        public age: number,
+        public isMarried?: boolean
+    ) { }
 };
 const friendData = new Root("Sabbir", "Ahmed", 25, false);
 console.log(friendData);
+
+class Circle extends Root {
+    total: number;
+    constructor(fName: string, lName: string, age: number, isMarried: boolean,total:number) {
+        super(fName, lName, age, isMarried);
+
+    }
+};
+const fData = new Circle("ikram", "akbar", 66, true, 2);
+console.log(fData);
+
 
